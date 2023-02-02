40adf64a02477f7170bd2c068301bf98758e36f9
Souvick Saha <souvicksaha01@gmail.com>
2023-02-02 15:17:56 +0530

    Selection sort Commited

--- First.java
+++ First.java
@@ -3,17 +3,48 @@ class First
    public static void main(String []args)
   {
          int []arr={30,20,10,50};
-        for(int i=0;i<4;i++)
+        //bubble sort
+       /* for(int i=0;i<4;i++)
         {
-              for(int j=0;j<4;j++)
+              for(int j=1;j<4;j++)
              {
-                  if(arr[j]>arr[j+1])
+                  if(arr[j-1]>arr[j])
                   {
-                          int temp=arr[j];
-                         arr[j]=arr[j+1];
-                         arr[j+1]=temp;
+                          int temp=arr[j-1];
+                         arr[j-1]=arr[j];
+                         arr[j]=temp;
                  }
              }
-        }
+        }*/
+      //insertion sort
+    /* int key;
+      for(int i=1;i<4;i++)
+     {
+          key=arr[i];
+          int j=i-1;
+         while(j>=0 && arr[j]>key)
+         {
+               arr[j+1]=arr[j];
+               j=j-1;
+          }
+       arr[j+1]=key;
+     }*/
+   //Selection code
+  for(int i=0;i<4;i++)
+  {
+       int index=i;
+       for(int j=i+1;j<4;j++)
+       {
+                if(arr[j]<arr[index])
+                      index=j;
+       }
+      int temp=arr[index];
+      arr[index]=arr[i];
+     arr[i]=temp;
+  }
+    
+      for(int i=0;i<4;i++)
+           System.out.println(arr[i]);
+      
    }
 }
